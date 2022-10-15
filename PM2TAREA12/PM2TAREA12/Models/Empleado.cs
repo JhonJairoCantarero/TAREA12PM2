221c82cb4ad75af7d41c69aaba22de40b60b0827
cantarerocantarero@gmail.com <cantarerocantarero95@gmail.com>
2022-10-14 23:38:11 -0600

    Proyecto Creado

---
+++ PM2TAREA12/PM2TAREA12/Models/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2TAREA12.Models
+{
+    internal class Empleado
+    {
+        public int Id { get; set; }
+        public string nombres { get; set; }
+
+        public string apellidos { get; set; }
+
+        public int edad { get; set; }
+
+        public string correo { get; set; }
+
+
+    }
+}
---
+++ PM2TAREA12/PM2TAREA12/Views/PageTwo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2TAREA12.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+            //HOLA
+
+        }
+
+        public PageTwo(String pmessage)
+        {
+            InitializeComponent();
+            lbcorreo.Text = pmessage;
+        }
+    }
+}
